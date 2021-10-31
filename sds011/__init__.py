493e5b4c8e273c9bb854854c2c4e0af0507d96e0
Flavio Pace <flavio.pace@gmail.com>
2021-10-31 22:45:52 +0100

    fix path

--- sds011/__init__.py
+++ sds011/__init__.py
@@ -5,6 +5,8 @@ import serial
 import aqi
 import time
 import json
+import os
+import sys
 from db_connection import mydb
 from thingspeak import Thingspeak
 from aqiUpload import Aqicn
@@ -189,7 +191,7 @@ if __name__ == "__main__":
     # Sleep 15 seconds
     time.sleep(15)
     # load Config
-    with open(JSON_FILE, 'r') as in_file:
+    with open(os.path.join(sys.path[0], JSON_FILE), 'r') as in_file:
         conf = json.load(in_file)
     # Init DB
     db = mydb(host=conf['db_config']['host'], name=conf['db_config']['name'], port=conf['db_config']['port'],\
